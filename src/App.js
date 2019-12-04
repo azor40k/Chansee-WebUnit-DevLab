7b91b0c0252a88fc04bce6f535f07226c8bf4ab9
axel <carandang.axel@gmail.com>
2019-12-04 09:27:39 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './components-r/Navbar';
+import Banner from './components-r/Banner';
+import Rule from './components-r/Rule';
+import Footer from './components-r/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Navbar />
+      <Banner />
+      <Rule />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components-r/Rule.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './Rule.css';
+
+class Rule extends React.Component {
+    render(){
+        return(
+
+            <div class="container-rule">
+                
+                <h1>Règles</h1>
+
+                <div class="block-rule">
+
+                    <h2 class="title-rule">Règles #X </h2>
+
+                    <p class="parag-rule"> Lorem ipsum dolor sit amet, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                
+                </div>
+
+                <div class="block-rule">
+
+                    <h2 class="title-rule">Règles #X </h2>
+
+                    <p class="parag-rule"> Lorem ipsum dolor sit amet, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                
+                </div>
+
+                <div class="block-rule">
+
+                    <h2 class="title-rule">Règles #X </h2>
+
+                    <p class="parag-rule"> Lorem ipsum dolor sit amet, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                
+                </div>
+
+                <div class="block-rule">
+
+                    <h2 class="title-rule">Règles #X </h2>
+
+                    <p class="parag-rule"> Lorem ipsum dolor sit amet, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                
+                </div>
+
+                <div class="block-rule">
+
+                    <h2 class="title-rule">Règles #X </h2>
+
+                    <p class="parag-rule"> Lorem ipsum dolor sit amet, Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                
+                </div>    
+            
+            
+            <br></br>
+            </div>
+
+        );
+    }
+}
+
+
+export default Rule;
